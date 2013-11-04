dbc30e7ba43018f7fe186f97f74705d88bca1ed8
Colin Shea <colin@evaryont.me>
2013-11-04 00:21:02 -0700

    Delete the generated secrets after each run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,9 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 # Where secretbox will store it's generated values during testing. It'll be this
 # directory + "/secretbox", so only go to the parent directory
 SECRETS_FIXTURES_DIR = File.expand_path '../fixtures/', __FILE__
+
+RSpec.configure do |config|
+  config.after(:each) do
+    FileUtils.rm_rf `git ls-files --others spec/fixtures/secretbox/localhost`.split("\n")
+  end
+end
